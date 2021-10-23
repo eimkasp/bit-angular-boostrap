6adfc17d8cabfb869e8eb5a3b5ff288f5a395b36
Eimantas <eim.kasp@gmail.com>
2021-10-23 13:30:31 +0300

    Duomenu perdavimas is product-list i product-card komponenta

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
+  // Kintamojo aprasymas, kurio duomenys bus paduodami is parent komponento
+  @Input() product : any ;
 
   constructor() { }
 
   ngOnInit(): void {
+    // Atspausdiname kintamojo reiksme i konsole
+    console.log(this.product);
   }
 
 }
